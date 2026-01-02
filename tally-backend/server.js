793cee303e5c139d792472fbc3b30846a851a5d9
ayrtonrajlaxmi-cell <ayrtonrajlaxmi@gmail.com>
2026-01-02 10:38:49 +0530

    Fix server.js  number line

--- tally-backend/server.js
+++ tally-backend/server.js
@@ -49,11 +49,16 @@ return next(); // TEMP: allow agent
 app.use(
   "/voucher-entry",
   (req, res, next) => {
-    if (req.path === "/sync" || req.path === "/mark-inactive") return next();
+    if (
+      req.path === "/sync" ||
+      req.path === "/mark-inactive" ||
+      req.path === "/bulk-sync"   // 🔥 THIS WAS MISSING
+    ) return next();
     requireAuth(req, res, next);
   },
   vouchersRoute
 );
+
 
 
 
